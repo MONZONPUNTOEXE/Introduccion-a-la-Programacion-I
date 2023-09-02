21a0a39189bf05cf56f487168326f31bba0a24b1
Netbook-loca <monzonfranco95@gmail.com>
2023-09-02 13:54:58 -0300

    trabajos corregidos xD

--- Unidad I y II/Tp1_2(corregido).py
+++ Unidad I y II/Tp1_2(corregido).py
@@ -1,10 +1,10 @@
-n = 0
+# Declaramos variables
+numero = 0
 maximo = 0
 pos = 0
 
-
-
 for i in range(0,3):
+    # No prestar atencion a esto, es una cosa que hice aparte
     if i == 0 : print("Ingrese su Primer número (puede ser negativo o positivo)")
     if i == 1 : print("Ingrese su Segundo número (puede ser negativo o positivo)")
     if i == 2 : print("Ingrese su Tercer número (puede ser negativo o positivo)")
@@ -15,10 +15,19 @@ for i in range(0,3):
     if i == 7 : print("Ingrese su Octavo número (puede ser negativo o positivo)")
     if i == 8 : print("Ingrese su Noveno número (puede ser negativo o positivo)")
     if i == 9 : print("Ingrese su Decimo número (puede ser negativo o positivo)")
-    
-    n = int(input())
-    if i == 0 : maximo = n
-    elif n > maximo : pos = i + n
+# Numero que va a ingresar el cliente
+    numero = int(input())
+    # Si el numero es igual a 0 que me lo guarde como maximo
+    if numero == 0 :
+        maximo = numero
+        # Me guarda la posicion de ese numero
+        pos = i + 1
+        # Si el numero no es igual a 0 que condicione si el numero es mayor 
+    elif numero > maximo : 
+        # si el numero es mayor que me lo guarde en Maximo y la posicion del mismo en Pos
+        maximo = numero
+        # Le agrego un + 1 paorque i empieza a contar desde el 0 y no desde el 1
+        pos = i + 1
 
     
 print("El Número más alto que ingreso fue el numero", maximo, "la posicion es", pos)

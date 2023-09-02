d1faf5c43fe620e6af185dc746ee6e2afda39f1f
Netbook-loca <monzonfranco95@gmail.com>
2023-09-02 14:11:01 -0300

    Tarea clase 4

---
+++ Clase-4-tarea-A.py
@@ -0,0 +1,6 @@
+# A)  Escribe un programa que permita al usuario ingresar 
+# un conjunto de 10 números a través del teclado. 
+# El programa debe determinar el número máximo en el conjunto, 
+# indicar su posición en la secuencia y
+# encontrar el segundo número más grande junto con su posición.
+
---
+++ Clase-4-tarea-B.py
@@ -0,0 +1,9 @@
+# B) Diseña un sistema de gestión para calcular los sueldos de los empleados de una empresa.
+# Cada empleado tiene un sueldo base de 80000 unidades.
+# Sin embargo, sus sueldos pueden variar según sus ventas y las comisiones que generen.
+# Si las ventas totales de un empleado alcanzan 10000 unidades, se le suma un 5% al sueldo base.
+# Si las ventas están en el rango de 10001 a 20000 unidades, la suma es del 9%. 
+# Para ventas que superen las 20001 unidades, la suma es del 15%. 
+# El sistema debe pedir el nombre del empleado y el total de ventas, 
+# y mostrar en pantalla el sueldo bruto resultante.
+

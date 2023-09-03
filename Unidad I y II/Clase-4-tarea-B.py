9a74d6d6fa65e2d0917d6d08dbfce3e75417838a
Netbook-loca <monzonfranco95@gmail.com>
2023-09-03 15:38:17 -0300

    Tarea Finalizada clase 4 (Falta Corregir A)

--- Unidad I y II/Clase-4-tarea-B.py
+++ Unidad I y II/Clase-4-tarea-B.py
@@ -28,11 +28,9 @@ if cantidad_de_ventas >= 10000 and cantidad_de_ventas < 10001:
     sueldo_total = sueldo_base + sueldo_base * 5 / 100
 if cantidad_de_ventas >= 10001 and cantidad_de_ventas <= 20000 :
     mensaje_del_porcentaje = "Por lo tanto se le aumento un 9 porciento mas al sueldo.";
-    print("-------------2");
     cantidad_de_ventas = cantidad_de_ventas
     sueldo_total = sueldo_base + sueldo_base * 9 / 100
 if cantidad_de_ventas >= 20001:
-    print("-------------3");
     mensaje_del_porcentaje = "Por lo tanto se le aumento un 15 porciento mas al sueldo.";
     sueldo_total = sueldo_base + sueldo_base * 15 / 100
 

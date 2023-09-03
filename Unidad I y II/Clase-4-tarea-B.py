188ca0a5e226e9d7cea623858b2e415d0c5a6991
Netbook-loca <monzonfranco95@gmail.com>
2023-09-03 15:35:46 -0300

    Tarea-B-Clase-4-realizada(falta la A)

--- Unidad I y II/Clase-4-tarea-B.py
+++ Unidad I y II/Clase-4-tarea-B.py
@@ -13,16 +13,27 @@ empleado = 0
 cantidad_de_ventas = 0
 sueldo_base = 80000
 sueldo_total = 0
+mensaje_del_porcentaje = None
 
 
 
 empleado = input("introduzca el nombre del empleado: ");
 cantidad_de_ventas = int(input("Introduzca el monto de las ventas: "));
 
-sueldo_total = sueldo_base * 5 / 100 
-print(sueldo_total);
-# if cantidad_de_ventas > 10000 :
-#     sueldo_base = sueldo_base * 5 / 100
+if cantidad_de_ventas < 10000:
+    mensaje_del_porcentaje = "No cumple con las condiciones para el bonus.";
+    sueldo_total = sueldo_base
+if cantidad_de_ventas >= 10000 and cantidad_de_ventas < 10001:
+    mensaje_del_porcentaje = "Por lo tanto se le aumento un 5 porciento mas al sueldo.";
+    sueldo_total = sueldo_base + sueldo_base * 5 / 100
+if cantidad_de_ventas >= 10001 and cantidad_de_ventas <= 20000 :
+    mensaje_del_porcentaje = "Por lo tanto se le aumento un 9 porciento mas al sueldo.";
+    print("-------------2");
+    cantidad_de_ventas = cantidad_de_ventas
+    sueldo_total = sueldo_base + sueldo_base * 9 / 100
+if cantidad_de_ventas >= 20001:
+    print("-------------3");
+    mensaje_del_porcentaje = "Por lo tanto se le aumento un 15 porciento mas al sueldo.";
+    sueldo_total = sueldo_base + sueldo_base * 15 / 100
 
-
-print("El empleado:", empleado, "realizo" , cantidad_de_ventas, "pesos en ventas");
+print("El empleado:", empleado, "realizo un total en ventas de:" , cantidad_de_ventas,"pesos.", mensaje_del_porcentaje, "Su sueldo actual es de:",int(sueldo_total),"pesos");

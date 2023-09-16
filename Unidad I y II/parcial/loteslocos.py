b8f919c30329de9a6c4fccbe24ed533bead8e265
Netbook-loca <monzonfranco95@gmail.com>
2023-09-16 20:33:29 -0300

    Agregando clase 2

---
+++ Unidad I y II/parcial/loteslocos.py
@@ -0,0 +1,45 @@
+## Esto es negocio
+## Ingresar lotes y agarrar el maximo de los do
+
+# 
+#2, 44, 66, 44
+#
+#
+
+def maximoLotes():
+    lote = 0
+    numero = 0
+    while lote != 2:
+        numero = int(input('ingrese un numero'))
+        if numero !=0:
+            # logica de lote
+            # identificar de lote es ??
+            if lote == 0:
+                if numero > lotemaximo
+                    maximolote = numero                                    
+            else: ## trabajamso con el segundo numero
+        else:            
+            lote = lote + 1    
+    print('El numero maximo del primer lote es , ', maxiLoteUno, ' El segundo del maximo lote es ', maximoLoteDos)
+
+
+
+
+
+
+
+
+# mayor = 0
+# lote1 = 0
+# lote2 = 0
+
+
+# while numero != 0:
+#     numero = int(input("ingrese un numero"))
+#     if numero > mayor :
+#         numero = mayor
+#         lote1 = mayor
+
+
+
+# print(lote1);
---
+++ Unidad I y II/parcial/negocio.py
@@ -0,0 +1,16 @@
+## Funcionalidad dentro de negocio
+def saludar():
+    nombre = input("Cual es tu nombre ?");
+    print("Bienvenido", nombre);
+
+def IngreseUnNumero():
+    numero = int(input("Ingrese un numero"))
+    print("tu numero es", numero)
+
+def mayor():
+    for i in range (0, 10):
+        int(input("Ingrese un numero"))
+        if numero > mayor:
+            numero = mayor
+            print()
+            

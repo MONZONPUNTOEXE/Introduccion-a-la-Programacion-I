8aeb48c8bf728237bb33e7238a6a5575868868e0
Netbook-loca <monzonfranco95@gmail.com>
2023-09-17 18:39:18 -0300

    punto 3 a medio finalizar

---
+++ Unidad I y II/Ejercicios-de-parcial/Punto_3/main.py
@@ -0,0 +1,18 @@
+import utils
+import negocio
+
+user = 0
+
+print("\n ***** Bienvenido, ¿que desea hacer? *****")
+while user != 3:
+    utils.imprimirMenu()
+    user = int(input("Ingrese un numero para acceder al menú "))
+    if user == 1:
+        negocio.sistemaAlfajores();
+    if user == 2:
+        print(user);
+    if user == 3:
+        print("Adios");
+    else: 
+        print("----- Opcion incorrecta ------ \n ------ Vuelva a intentarlo -----")
+
---
+++ Unidad I y II/Ejercicios-de-parcial/Punto_3/negocio.py
@@ -0,0 +1,29 @@
+def sistemaAlfajores():
+    i = 1
+    unidad = 0
+    caja = 0
+    unidad_precio = 0.5
+    caja_precio = 5
+
+    cant_alfajores = int(input("Ingrese la cantidad de alfajores "));
+
+    while i <= cant_alfajores:
+        if i % 12 == 0:
+            caja += 1
+        elif i % 12 != 0:
+            unidad += 1
+        i += 1
+
+    unidades = cant_alfajores - caja * 12
+    subtotal_caja = caja * caja_precio
+    subtotal_unidades = unidades*unidad_precio
+    total = subtotal_caja + subtotal_unidades
+
+    print("unidades =", unidades,"caja =", caja);
+    print("El cliente compró por caja la cantidad de:", caja, "\n" "Por unidad la cantidad de:", unidades);
+    print("Por lo tanto el subtotal por caja es de:", subtotal_caja,"Pesos" "\n" "y por unidad es de:", subtotal_unidades, "Pesos");
+    print("********************* El total a pagar es de:", total, "*********************" )
+
+def sistemaAlfajoresMayor():
+    import 
+    print(total)

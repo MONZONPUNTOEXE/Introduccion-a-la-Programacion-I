6bf37efaed3fa32e446e52ad90352848402af7f7
Netbook-loca <monzonfranco95@gmail.com>
2023-09-17 16:17:56 -0300

    ejercicio 2 terminado

--- Unidad I y II/Ejercicios-de-parcial/Punto_2/main.py
+++ Unidad I y II/Ejercicios-de-parcial/Punto_2/main.py
@@ -1,12 +1,18 @@
 salario_xhora = int(input("Ingrese su salario por hora: "));
-
 horas_trabajadas = int(input("Ingrese sus horas trabajadas de esta semana: "));
+total = 0
 
-subtotal = salario_xhora * horas_trabajadas
 
-if horas_trabajadas >= 40:
+if horas_trabajadas > 40:
     horas_extras = horas_trabajadas - 40
     print(horas_extras)
+    subtotal = horas_extras * salario_xhora  * 10 / 100
+    total = horas_trabajadas * salario_xhora + subtotal
+    print(int(total))
+elif horas_trabajadas < 40 :
+    total = horas_trabajadas*salario_xhora
+
+print("Tu salario es de ",total, "pesos");
 
 
     

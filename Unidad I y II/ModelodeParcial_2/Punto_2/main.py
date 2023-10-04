3444bf28fff1fa91ce8fe544e10b019480f01e51
Franco Monzon <monzonfranco95@gmail.com>
2023-10-03 22:52:39 -0300

    Corregi el modelo de parcial - Ejercicio 2

--- Unidad I y II/ModelodeParcial_2/Punto_2/main.py
+++ Unidad I y II/ModelodeParcial_2/Punto_2/main.py
@@ -5,18 +5,32 @@ posicion_2 = 0
 suma = 0
 
 for i in range (0,5):
-    user = int(input("Ingrese un numero"))    
-    if maximo < user:
+    user = int(input("Ingrese un numero mayor a cero: "))
+    # hacemos uso del "While", si el usuario ingresa un valor menor a cero no lo deje ingresar
+    # otro que no sea mayor a cero 
+    while user <= 0: # Mientras User sea "Verdadero" entrara en el bucle
+        print("*ERROR*\nPor favor ingrese un número mayor a cero: ")
+        # Es importante actualizar la variable "user". Porque sino queda con el valor que se le asigno
+        # y siempre va valer Verdadero y entra en el bucle siempre, creando un bucle infinito
+        # Para esto le decimos al usuario que ingrese un valor mayor a Cero. 
+        user = int(input("Ingrese un numero mayor a cero: "))
+        # Si el usuario ingresa un valor mayor a Cero, la condicional será Falsa, por lo tanto rompe el bucle
+    if maximo < user: # Si el dato es verdadeo ejecuta este codigo
         maximo2 = maximo
         maximo = user
         posicion_1 = i + 1
         posicion_2 = i
-    elif maximo2 < user:
+    elif maximo2 < maximo: # Si el dato en el "if" es Falso, entrara en el "elif" y ejecuta este código
         maximo2 = user
         posicion_2 = i + 1
 
+    # IMPORTANTE: En este caso es importante que si o si pongamos un "if" y luego un "elif" porque:
+    # 1) Si el dato de "if" es falso ira al "elif" y actualizara el maximo2 si maximo es mayor a maximo2
+    # 2) Si nosotros en vez de colocar "if" y "elif" colocamos "if" e "if" si el dato da Verdadero en ambos casos, 
+    #     sobreescribira la variable (maximo2). Porque el dato fue Verdadero en ambas condicionales a la vez.
+    #     Y en este caso no queremos eso 
 
-print(i)
+# Sumamos
 suma = maximo + maximo2
 
 print("El valor máximo ingresado es:",maximo,"\nEl valor del segundo máximo ingresado es:",maximo2,"\nLa posición del valor máximo es:",posicion_1,"\nLa posición del segundo valor máximo es:",posicion_2,"\nLa sumatoria total de los números ingresados es:", suma)
